60e99ecbd5fbff0bae461b844c4b458e74fbb524
PaulTadi <36857177+PaulTadi@users.noreply.github.com>
2018-03-05 12:35:15 -0500

    ajout d'adresse dans patient

--- dbBioMedic.sql
+++ dbBioMedic.sql
@@ -27,6 +27,7 @@ CREATE TABLE `tbpatient` (
   `prenom` varchar(50) DEFAULT NULL,
   `nom` varchar(50) DEFAULT NULL,
   `dateNaissance` date DEFAULT NULL,
+  `adresse` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
