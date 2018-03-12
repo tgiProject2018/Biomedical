c4e801893636b60b290690f21e1be28e111e24b3
PaulTadi <36857177+PaulTadi@users.noreply.github.com>
2018-03-12 12:49:47 -0400

    Update dbBioMedicSqlite.sql

--- dbBioMedicSqlite.sql
+++ dbBioMedicSqlite.sql
@@ -5,6 +5,7 @@ CREATE TABLE tblPatient (
 id integer primary key autoincrement,
 prenom text,
 nom text,
+adresse text,
 dateNaissance numeric);
 INSERT INTO tblPatient VALUES(1,'paul','tadi','1994-08-02');
 CREATE TABLE tblPriseTeste (
@@ -15,9 +16,6 @@ diasystolique real,
 poul real,
 idPatient integer,
 foreign key (idPatient) references tblPatient(id));
-INSERT INTO tblPriseTeste VALUES(1,1994,NULL,NULL,NULL,1);
-INSERT INTO tblPriseTeste VALUES(2,'2018-02-27',10.0,22.999999999999999999,44.0,1);
-INSERT INTO tblPriseTeste VALUES(3,1990,NULL,NULL,NULL,1);
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('tblPatient',1);
 INSERT INTO sqlite_sequence VALUES('tblPriseTeste',1989);
