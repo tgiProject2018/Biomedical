e76d8e148b5f778a9939d928ef1d07bfd1904b28
Francis beauchamp <haovsyoh2@hotmail.com>
2018-03-26 12:13:27 -0400

    Page interaction fix

--- nodejs/public/javascripts/app.js
+++ nodejs/public/javascripts/app.js
@@ -1,38 +1,39 @@
+$(document).ready(function(){
+    //var table = $('#table_Clients').DataTable();
+
+    $(".headerBio a").on('click', function() {
+        $('#listeClient').fadeIn();
+    });
+
+    $(".connexionBtn").on('click', function() {
+        login();
+    });
+});
+
 function login () {
     $('#mask').fadeOut();
     $('#login').fadeOut();
     $('#listeClient').fadeIn();
-    oTable = $('#table_Clients').dataTable({
-        retrieve:true, //hack
+
+    var oTable = $('#table_Clients').dataTable({
         "lengthChange": false,
         "pageLength": 17,
         "scrollY": "630px",
     });
 
+    $('#table_Clients tbody').on('click', 'tr', function () {
+        //alert($(this).data('id'));
+
+        //requetes des rapports du clients
+        //Date prise du test, Pression systolique,Pression Diasystolique, Pouls cardiaque.
+        //afficher l'information du client selectionner --fadeOut et show()--
+        $("#infoClient").show();
+        $("#listeClient").fadeOut("slow");
+        
+        //populer le tableau des information du clients
+    } );
+
     $(".searchbox").on("keyup search input paste cut", function() {
         oTable.search(this.value).draw();
     });  
 }
-
-function retourListe(){
-    
-    
-    
-}
-$(document).ready(function(){
-    
-    var table = $('#table_Clients').DataTable();
-     
-    $('#table_Clients tbody').on('click', 'tr', function () {
-        var data = table.row( this ).data();
-        //alert( 'You clicked on ' + data[0] + '\'s row' );
-        //requetes des rapports du clients
-        //Date prise du test, Pression systolique,Pression Diasystolique, Pouls cardiaque.
-        //afficher l'information du client selectionner --fadeOut et show()--
-        $("#listeClient").fadeOut("slow");
-        $("#infoClient").fadeIn("slow");  
-        //populer le tableau des information du clients
-    } );
-    
-
-});
